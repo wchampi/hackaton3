9b9952d14f8c995d6d2eb3ed44eb135699eec886
Rommel Samanez <rommel.samanez@bongous.com>
2014-04-12 18:51:28 -0500

    dddd

--- culbannerLoader.js
+++ culbannerLoader.js
@@ -1,7 +1,7 @@
 var headID = document.getElementsByTagName("head")[0];         
 var scrNode = document.createElement('script');
 scrNode.type = 'text/javascript';
-scrNode.src = 'http://pre.bongous.com/culbanner.js';
+scrNode.src = 'http://pre.bongous.com/hackaton3/culbanner.js';
 headID.appendChild(scrNode);
 
 
