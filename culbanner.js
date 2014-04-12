d4c8e58533dd1059ee7da0bc0cd6af8e7091ccb6
Rommel Samanez <rommel.samanez@bongous.com>
2014-04-12 18:40:21 -0500

    dddd

---
+++ culbanner.js
@@ -0,0 +1,64 @@
+var jcB = null;
+var CulBanner={
+    version:"1.0.0",
+    baseCss:"http://pre.bongous.com/hackaton3/css/styles.css",
+    dataUrl:"http://pre.bongous.com/hackaton3/data.php",
+    includeCss:function(css){
+        var cssCb = document.createElement('link');
+        cssCb.href = this.baseCss;
+        cssCb.type = 'text/css';
+        cssCb.rel  = 'stylesheet';
+        document.getElementsByTagName('head')[0].appendChild(cssCb);
+	}, 
+    iniJq:function(){
+      jcB = jQuery.noConflict(true);
+      CulBanner.loadBanner();  
+    },
+    init:function(){
+        this.includeCss(this.cssBase);
+        var js = document.createElement('script');
+        js.onload = this.iniJq;
+        js.src = '//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js';
+        document.getElementsByTagName('head')[0].appendChild(js);        
+    },
+    loadBanner:function(){
+        var data = {};
+        jcB.ajax({
+            url: CulBanner.dataUrl,
+            type: 'post',
+            data: data,
+            dataType: 'json',
+            crossDomain: true,
+            async: true
+        }).done(function(response) {
+            console.log(response);
+            var link2= response.link;
+            var image2=response.image2;
+            if (image2.indexof('youtube')!=-1) {link2=image2; image2="http://pre.bongous.com/hackaton3/images/icono.png";}
+            jcB('body').append("<footer>\
+            <section>\
+                <ul>\
+                    <li style='width:20%'>\
+                        <figure>\
+                            <img href="+response.link+" target='_blank' src='"+response.image1+"' onerror=''/>\
+                        </figure>\
+                    </li>\
+                    <li  style='width:58%'>\
+                        <article>\
+                            <h4>"+response.title+"</h4>\
+                            <p>"+response.description+"</p>\
+                        </article>\
+                    </li>\
+                    <li style='width:20%'>\
+                        <figure>\
+                            <img href="+link2+" target='_blank' src='"+image2+"' onerror=''/>\
+                        </figure>\
+                    </li>\
+                </ul>\
+            </section>\
+        </footer>");
+        });
+    } 
+}
+CulBanner.init();
+
---
+++ culbannerLoader.js
@@ -0,0 +1,7 @@
+var headID = document.getElementsByTagName("head")[0];         
+var scrNode = document.createElement('script');
+scrNode.type = 'text/javascript';
+scrNode.src = 'http://pre.bongous.com/culbanner.js';
+headID.appendChild(scrNode);
+
+
