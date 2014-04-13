1c95ee329f21e60e6b7bab4e12c188e6c70cdcf1
Wilson Champi <wchampi86@gmail.com>
2014-04-13 11:27:39 -0500

    embed youtube

--- data.php
+++ data.php
@@ -1,5 +1,25 @@
 <?php
 
+function getUrl2($url) {
+    if (strpos($url, 'youtube.com') !== false || strpos($url, 'youtu.be') !== false) {
+        if (strpos($url, 'youtube.com/embed') !== false) {
+            return $url;
+        }
+        
+        if (strpos($url, 'youtu.be') !== false) {
+            $pUrl = explode('youtu.be/', $url);
+            $v = $pUrl[1];
+        } else {
+            $pUrl = parse_url($url);
+            parse_str($pUrl['query'], $pString);
+            $v = $pString['v'];
+        }
+
+        return '//www.youtube.com/embed/' . $v;
+    }
+    return $url;
+}
+
 header('Access-Control-Allow-Origin: *');  
 header('Access-Control-Allow-Methods: POST, GET');
 header('Access-Control-Allow-Credentials: true');
@@ -17,7 +37,7 @@ $response = array(
     'title' => $evento['nombre_evento'],
     'description' => $evento['resumen'],
     'image1' => ($evento['link_image'] == '') ? 'http://www.publicatufoto.com/data/media/44/Municipalidad_de_Lima.PNG' : $evento['link_image'],
-    'image2' => ($evento['link_youtube'] == '') ? 'http://www.publicatufoto.com/data/media/44/Municipalidad_de_Lima.PNG' : $evento['link_youtube'],
+    'image2' => ($evento['link_youtube'] == '') ? 'http://www.publicatufoto.com/data/media/44/Municipalidad_de_Lima.PNG' : getUrl2($evento['link_youtube']),
     'link' => ($evento['link'] == '') ? 'http://www.munlima.gob.pe' : $evento['link']
 );
 
