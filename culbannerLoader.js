35e483d1d0d695b70c1d396226ec2dae86e2b797
Rommel Samanez <rommel.samanez@bongous.com>
2014-04-12 20:00:21 -0500

    ya me canse

--- culbannerLoader.js
+++ culbannerLoader.js
@@ -1,7 +1,7 @@
 var headID = document.getElementsByTagName("head")[0];         
 var scrNode = document.createElement('script');
 scrNode.type = 'text/javascript';
-scrNode.src = 'http://pre.bongous.com/hackaton3/culbanner.js?e';
+scrNode.src = 'http://pre.bongous.com/hackaton3/culbanner.js?f';
 headID.appendChild(scrNode);
 
 
