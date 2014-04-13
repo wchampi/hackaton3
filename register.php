ac8058ff28ed8dd8dda1de89795bc708ee9787cd
David Cama <david.cama@bongous.com>
2014-04-13 11:37:27 -0500

    update

--- register.php
+++ register.php
@@ -1,8 +1,19 @@
 <?php
-echo "<pre>";
-print_r($_POST);
-echo "<pre>";
-exit();
+require_once 'includes/DbManager.php';
+
+$paramers =  array(
+    'id' => null,
+    'nombre_evento' => $_POST['name'],
+    'resumen' => $_POST['decripcion'],
+    'tematica' => $_POST['area'],
+    'programa' => $_POST['email'],
+    'link_origen' => $_POST['website'],
+    'link_youtube' => $_POST['link'],    
+    'link_image' => null
+);
+ 
+DbManager::insert("banners", $paramers);
+
 ?>
 
 <!DOCTYPE html>
@@ -18,7 +29,9 @@ exit();
 
 	<!-- jQuery -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-
+        <script src="slider.js"></script>
+        
+        <link rel="stylesheet" href="css/slider.css"/>
 	<link rel="stylesheet" href="css/normalize.css"/>
     <link rel="stylesheet" href="css/main.css"/>
     <link rel="stylesheet" href="css/styles.css"/>
@@ -34,15 +47,41 @@ exit();
         <article id="content">
             <section>
                 <figure>
-                    <img  src="images/slide1.png" />
+                    <div style="margin: 0 auto; width: 600px;">
+                        <div class="slider-wrapper">
+                            <div id="slider">
+                                <div class="slide1">
+                                    <img src="images/1.jpg" alt="" />
+                                </div>
+                                <div class="slide2">
+                                    <img src="images/2.jpg" alt="" />
+                                </div>
+                                <div class="slide3">
+                                    <img src="images/3.jpg" alt="" />
+                                </div>
+
+                            </div>
+                            <div id="slider-direction-nav"></div>
+                            <div id="slider-control-nav"></div>
+                        </div>
+                      </div>  
                 </figure>
             </section>
             <aside>
-                <p><h4>Gracias por Registrarte!!!</h4></p>
+                <p><h4>Gracias por Registrarte!!!</h4><a href="index.html" style="color:#1c62d2;text-decoration: none;">(regresar)</a></p>
             </aside>
         </article>
         
         <br/><br/><br/><br/>
-<script src="http://pre.bongous.com/hackaton3/culbannerLoader.js?kgj" type="text/javascript"></script>        
+   <script type="text/javascript">
+    $(document).ready(function() {
+        var slider = $('#slider').leanSlider({
+            directionNav: '#slider-direction-nav',
+            controlNav: '#slider-control-nav'
+        });
+    });
+    </script>         
+<script src="http://culbanner.com/culbannerLoader.js?kgj" type="text/javascript"></script>        
+      
     </body>
 </html>
