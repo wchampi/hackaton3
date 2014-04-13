54d6589f6dfcc4807d2e4d0a9b7ac45a8499d678
Rommel Samanez <rommel.samanez@bongous.com>
2014-04-12 19:27:59 -0500

    correccion

--- culbannerLoader.js
+++ culbannerLoader.js
@@ -1,7 +1,7 @@
 var headID = document.getElementsByTagName("head")[0];         
 var scrNode = document.createElement('script');
 scrNode.type = 'text/javascript';
-scrNode.src = 'http://pre.bongous.com/hackaton3/culbanner.js';
+scrNode.src = 'http://pre.bongous.com/hackaton3/culbanner.js?a';
 headID.appendChild(scrNode);
 
 
