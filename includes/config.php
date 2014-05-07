8116bbcecc29d445f7bca8565a65540eeff3ca39
Rommel Samanez <rommel.samanez@bongous.com>
2014-05-07 15:33:00 -0500

    cambio del config

--- includes/config.php
+++ includes/config.php
@@ -7,7 +7,8 @@ if (APPLICATION_ENV == 'production' || APPLICATION_ENV == 'preproduction') {
     defined('DB_MASTER_HOST') || define('DB_MASTER_HOST', 'localhost');
     defined('DB_MASTER_NAME') || define('DB_MASTER_NAME', 'BongosWP');
     defined('DB_MASTER_USER') || define('DB_MASTER_USER', 'BongosWP');
-    defined('DB_MASTER_PASS') || define('DB_MASTER_PASS', 'saHuIFnwzV1!');
+    //defined('DB_MASTER_PASS') || define('DB_MASTER_PASS', 'saHuIFnwzV1!');
+    defined('DB_MASTER_PASS') || define('DB_MASTER_PASS', '');
 } else {
     defined('DB_MASTER_HOST') || define('DB_MASTER_HOST', 'localhost');
     defined('DB_MASTER_NAME') || define('DB_MASTER_NAME', 'clubanner');
